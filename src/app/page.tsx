27019f770208b12eac3d1dbe5ddc7026c7100093
MeghanaRitu <meghana.ritu.feb17@gmail.com>
2025-03-21 01:08:29 +0530

    Fix PageProps type for searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 // HomePage is a SERVER COMPONENT (fetches data)
 import { getBlogPosts } from "@/lib/contentfulGraphQL";
-import BlogList from "@/components/BlogList"; 
+import BlogList from "@/components/BlogList";
+import { PageProps } from "next"; 
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams?: Record<string, string | string[]>;
-}) {
+export default async function HomePage({ searchParams }: PageProps) {
   const locale = searchParams?.lang?.toString() || "en-US";
   const posts = await getBlogPosts(locale);
 
