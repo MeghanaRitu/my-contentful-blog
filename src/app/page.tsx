7a91745218d20a3e6aca553728c602a134e1d494
MeghanaRitu <meghana.ritu.feb17@gmail.com>
2025-03-21 01:12:53 +0530

    Fix searchParams typing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-// HomePage is a SERVER COMPONENT (fetches data)
 import { getBlogPosts } from "@/lib/contentfulGraphQL";
 import BlogList from "@/components/BlogList";
-import { PageProps } from "next"; 
 
-export default async function HomePage({ searchParams }: PageProps) {
+export default async function HomePage({
+  searchParams,
+}: {
+  searchParams?: Record<string, string | string[]>;
+}) {
   const locale = searchParams?.lang?.toString() || "en-US";
   const posts = await getBlogPosts(locale);
 
